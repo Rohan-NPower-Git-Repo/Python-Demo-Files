633ff9f27a55409a23bf055c0b5c4ae019496600
Rohan-NPower-Git-Repo <rohan.deshpande@npowercanada.ca>
2024-11-28 10:31:44 -0500

    Code Files added

---
+++ Py-Variable-Arith.py
@@ -0,0 +1,34 @@
+# Python Comment starts with a # and comments are never executed they are used for information
+
+# Variable is nothing but a place to store your data
+
+# Store integers
+num = 123 
+print( 'Value : ', num, '\t Type : ', type(num) )
+
+# Constant's in Capitals is a Convention
+# Store floating point values
+PI = 3.141592 
+print( 'Value : ', PI, '\t Type : ', type(PI) )
+
+# Store Strings - Series of Characters
+city = 'Toronto' 
+print( 'Value : ', city, '\t Type : ', type(city) )
+
+# Store Boolean Values True or False
+isValid = True 
+print( 'Value : ', isValid, '\t Type : ', type(isValid) )
+
+# Basic Calculator - Arithmetic Operations
+num1 = 20
+num2 = 7
+
+print( 'Basic Calculator:' )
+print( '\t Addition = ', num1 + num2 )
+print( '\t Subtraction = ', num1 - num2 )
+print( '\t Multiplication = ', num1 * num2 )
+print( '\t Division = ', num1 / num2 )
+print( '\t Remainder = ', num1 % num2 )
+print( '\t Power = ', num1 ** num2 )
+# the floor division // rounds the result down to the nearest whole number 
+print( '\t Floor Division = ', num1 // num2 )
